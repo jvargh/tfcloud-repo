016779ffc4da7a7597923da4dcef88572a3cfc2f
JV <joji_varghese@hotmail.com>
2021-10-31 00:23:41 -0400

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  type = map(any)
+  default = {
+    default = "t2.nano"
+    dev     = "t2.micro"
+    prod    = "t2.large"
+  }
+}
+
+resource "aws_instance" "webapp" {
+  ami           = "ami-0fcf12ccffe66ff1c" 
+  instance_type = lookup(var.instance_type, 'dev')
+  tags = {
+    Name = "ec2-tf"
+  }
+}
